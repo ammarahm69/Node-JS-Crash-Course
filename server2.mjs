5adb2985dd27a88e08981a59c435a82f5292eb0b
Ammar Ahmed <ammar69aa59@gmail.com>
2024-08-11 02:44:04 -0700

    user-id succefully get

--- server2.mjs
+++ server2.mjs
@@ -1,45 +1,37 @@
-import { log } from "console";
 import { createServer } from "http";
 const PORT = process.env.PORT;
+//User object
 const users = [
-  {
-    id: 1,
-    name: "Ammar",
-    age: "20",
-  },
-  {
-    id: 2,
-    name: "Burhan",
-    age: "19",
-  },
-  {
-    id: 3,
-    name: "Taha",
-    age: "30",
-  },
+  { id: 1, name: "Maham" },
+  { id: 2, name: "Wara" },
+  { id: 3, name: "Jatoi" },
 ];
+
+/// Create a server
 const server = createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(users));
-    res.end();
-  } 
-  //Getting users with its id
-  else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-    const id = req.url.split("/")[3];
-    
-    console.log(id);
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ message: "Ayeza" }));
-    res.end();
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify(users));
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+    const id = req.url.split("/").pop();
+    const user = users.find((users) => users.id === parseInt(id));
+    if (user) {
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end(JSON.stringify(user));
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(404);
+      res.end(JSON.stringify({ message: "User not found" }));
+    }
   } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.write("Not Found");
-    res.end();
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("Page Not Found");
   }
 });
 
+//server listening on PORT
 server.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
 });
